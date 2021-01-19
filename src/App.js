5054cd2c91ad5b624fdfd23768a392e3cc680d6c
keith.quinn <keithqui@uk.ibm.com>
2021-01-19 10:50:36 +0000

    change app test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jamesy boy!
+          Hey Timmy, I have a surprise for you...
         </p>
         <a
           className="App-link"
